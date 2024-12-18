b9a80d1f23b958061a72b04c036603b0785ce1cd
Nadir Ahmed <itna@AMAUK.local>
2024-12-18 18:04:02 +0000

    Add gh actions

--- styles/vite.config.js
+++ styles/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Wetter.0/",
+  base: "/WetterX/",
   plugins: [react()],
 });
